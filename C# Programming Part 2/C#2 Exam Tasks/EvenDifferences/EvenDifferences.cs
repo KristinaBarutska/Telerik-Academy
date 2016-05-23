2037b4e78674e2f5376c9f5ee6cdbe2a8cbe8c32
Gabriel Belchev <belchev.gb@gmail.com>
2016-05-23 12:04:39 -0700

    EvenDifferences solved

---
+++ C# Programming Part 2/C#2 Exam Tasks/EvenDifferences/EvenDifferences.cs
@@ -0,0 +1,44 @@
+﻿namespace EvenDifferences
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public class EvenDifferences
+    {
+        private static long[] sequence;
+
+        public static void Main()
+        {
+            ReadInput();
+            
+            PrintResult();
+        }
+
+        private static void PrintResult()
+        {
+            BigInteger sumOfEvenDifferences = 0;
+
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                long currentDifference = Math.Abs(sequence[i] - sequence[i - 1]);
+
+                if (currentDifference % 2 == 0)
+                {
+                    sumOfEvenDifferences += currentDifference;
+                    i++;
+                }
+            }
+
+            Console.WriteLine(sumOfEvenDifferences);
+        }
+        
+        private static void ReadInput()
+        {
+            sequence = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+        }
+    }
+}
