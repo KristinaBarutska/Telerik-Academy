abd858fdae27fa52de41a46f301b83648a499390
Gabriel <belchev.gb@gmail.com>
2016-06-08 08:27:53 -0700

    Task-4 - Typeof Null

---
+++ JavaScript Fundamentals/DataTypes and Variables/TypeofNull/script.js
@@ -0,0 +1,13 @@
+/*
+ Problem 4. Typeof null
+ Create null, undefined variables and try typeof on them.
+ */
+
+(function () {
+    'use strict';
+
+    let nullable = null;
+    let undef;
+
+    console.log(typeof nullable + '\n' + typeof undef);
+} ());
