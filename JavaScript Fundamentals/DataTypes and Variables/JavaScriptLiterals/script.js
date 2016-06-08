24fbd55db96b0dc17e654dcd78a3b7b3cdb9c4b8
Gabriel <belchev.gb@gmail.com>
2016-06-08 08:25:11 -0700

    Task-1 JavaScript Literals

---
+++ JavaScript Fundamentals/DataTypes and Variables/JavaScriptLiterals/script.js
@@ -0,0 +1,16 @@
+/*
+ Problem 1. JavaScript literals
+ Assign all the possible JavaScript literals to different variables.
+ */
+
+(function () {
+    'use strict';
+
+    let number = 11,
+        str = 'str',
+        arr = [],
+        obj = {},
+        nullable = null,
+        bool = true,
+        undef;
+}());
