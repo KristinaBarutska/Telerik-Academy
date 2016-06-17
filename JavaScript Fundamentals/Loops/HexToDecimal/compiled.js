3128d4415c333b0c03092ca15271406f00d1287c
Gabriel <belchev.gb@gmail.com>
2016-06-17 10:32:33 -0700

    Hex to dec solved

---
+++ JavaScript Fundamentals/Loops/HexToDecimal/compiled.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function solve(input) {
+    'use strict';
+
+    var hex = '0x' + input[0];
+    var number = parseInt(hex);
+
+    console.log(number);
+}
---
+++ JavaScript Fundamentals/Loops/HexToDecimal/hex-to-decimal.js
@@ -0,0 +1,8 @@
+function solve(input) {
+    'use strict';
+
+    let hex = '0x' + input[0];
+    let number = parseInt(hex);
+    
+    console.log(number);
+}
