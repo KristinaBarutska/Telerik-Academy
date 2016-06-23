5cef892cd4c3192420970a1fa38f82c8fecfc5d0
Gabriel <belchev.gb@gmail.com>
2016-06-23 15:04:11 -0700

    Increase Array Members soled

---
+++ JavaScript Fundamentals/Arrays/IncreaseArrayMembers/compiled.js
@@ -0,0 +1,12 @@
+function solve(input) {
+    'use strict';
+
+    var n = Number(input[0]);
+    var numbers = new Array(n);
+
+    for (var i = 0; i < n; i += 1) {
+        numbers[i] = i * 5;
+    }
+
+    console.log(numbers.join('\n'));
+}
