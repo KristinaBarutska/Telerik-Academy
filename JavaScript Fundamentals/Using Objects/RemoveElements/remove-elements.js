79980aff8526ead3da586aa43239b1f53b41aee0
Gabriel <belchev.gb@gmail.com>
2016-06-24 11:50:46 -0700

    Remove element

---
+++ JavaScript Fundamentals/Using Objects/RemoveElements/remove-elements.js
@@ -0,0 +1,36 @@
+function solve(input) {
+    'use strict';
+
+    Array.prototype.remove = function (element) {
+        return [].filter.call(this, s => s !== element);
+    };
+
+    let elementToRemove = input[0];
+    input = input
+        .remove(elementToRemove)
+        .join('\n');
+    console.log(input);
+}
+
+solve([
+  '_h/_',
+  '^54F#',
+  'V',
+  '^54F#',
+  'Z285',
+  'kv?tc`',
+  '^54F#',
+  '_h/_',
+  'Z285',
+  '_h/_',
+  'kv?tc`',
+  'Z285',
+  '^54F#',
+  'Z285',
+  'Z285',
+  '_h/_',
+  '^54F#',
+  'kv?tc`',
+  'kv?tc`',
+  'Z285'
+]);
